8bbcae69984339c531f2a58809a31dfa0c65412a
Fernando J Pando <fernando.pando@stelligent.com>
2018-07-16 11:38:36 -0400

    workers

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -ex
 
-CFN_TEMPLATE=nomad_cluster.yml
 STACK_NAME=nomad-cluster
+VPC_STACK_NAME=${STACK_NAME}-vpc
+MASTER_STACK_NAME=${STACK_NAME}-masters
+WORKER_STACK_NAME=${STACK_NAME}-workers
+VPC_TEMPLATE=templates/vpc.yml
+MASTER_TEMPLATE=templates/masters.yml
+WORKER_TEMPLATE=templates/workers.yml
 AWS_PROFILE=default
 AWS_REGION=us-east-1
 IMAGE_ID=ami-97785bed  # latest amzn linux
-INSTANCE_TYPE=t2.large
+MASTER_INSTANCE_TYPE=t2.large
+WORKER_INSTANCE_TYPE=t2.large
 KEY_NAME=nomad
 VPC_CIDR=10.10.10.0/24
 SUBNET_CIDR=10.10.10.0/24
@@ -17,24 +23,52 @@ if [ -z ${TRUSTED_CIDR} ]; then
   exit 1
 fi
 
-PARAMETERS="\
-  ImageId=${IMAGE_ID} \
-  InstanceType=${INSTANCE_TYPE} \
-  KeyName=${KEY_NAME} \
+VPC_PARAMETERS="\
   VpcCidr=${VPC_CIDR} \
   SubnetCidr=${SUBNET_CIDR} \
   TrustedCidr=${TRUSTED_CIDR} \
+"
+
+MASTER_PARAMETERS="\
+  ImageId=${IMAGE_ID} \
+  InstanceType=${MASTER_INSTANCE_TYPE} \
+  KeyName=${KEY_NAME} \
   NomadVersion=${NOMAD_VERSION} \
   ConsulVersion=${CONSUL_VERSION} \
 "
 
-echo -e "\n\nDeploying ${STACK_NAME} Stack:\n\n"
+WORKER_PARAMETERS="\
+  ImageId=${IMAGE_ID} \
+  InstanceType=${WORKER_INSTANCE_TYPE} \
+  KeyName=${KEY_NAME} \
+  NomadVersion=${NOMAD_VERSION} \
+  ConsulVersion=${CONSUL_VERSION} \
+"
+
+echo -e "\n\nDeploying ${VPC_STACK_NAME} Stack:\n\n"
 aws cloudformation deploy \
+  --stack-name ${VPC_STACK_NAME} \
   --profile ${AWS_PROFILE} \
   --region ${AWS_REGION} \
-  --stack-name ${STACK_NAME} \
   --capabilities CAPABILITY_IAM \
-  --template-file ${CFN_TEMPLATE} \
+  --template-file ${VPC_TEMPLATE} \
   --no-fail-on-empty-changeset \
-  --parameter-overrides ${PARAMETERS}
+  --parameter-overrides ${VPC_PARAMETERS}
 
+echo -e "\n\nDeploying ${MASTER_STACK_NAME} Stack:\n\n"
+aws cloudformation deploy \
+  --stack-name ${MASTER_STACK_NAME} \
+  --profile ${AWS_PROFILE} \
+  --region ${AWS_REGION} \
+  --template-file ${MASTER_TEMPLATE} \
+  --no-fail-on-empty-changeset \
+  --parameter-overrides ${MASTER_PARAMETERS}
+
+echo -e "\n\nDeploying ${WORKER_STACK_NAME} Stack:\n\n"
+aws cloudformation deploy \
+  --stack-name ${WORKER_STACK_NAME} \
+  --profile ${AWS_PROFILE} \
+  --region ${AWS_REGION} \
+  --template-file ${WORKER_TEMPLATE} \
+  --no-fail-on-empty-changeset \
+  --parameter-overrides ${WORKER_PARAMETERS}
